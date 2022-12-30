182713a260942f4d9d32156c6ded3a350159d87d
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2022-12-30 23:38:09 +0100

    Set up the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
+import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				{/* set up favicon */}
+				<link rel="shortcut icon" href="favicon/favicon.svg" type="img/svg" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon/favicon-180x180.png" />
+          		<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+          		<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	)
 }
